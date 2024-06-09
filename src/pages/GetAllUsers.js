ed52929f9ddabf77f32160728984f1213fb33ca2
Yuneth <yunethperera987@gmail.com>
2024-06-09 09:54:57 +0530

    try to add ReportView

--- src/pages/GetAllUsers.js
+++ src/pages/GetAllUsers.js
@@ -1,1 +1,21 @@
-export default function GetAllUser() {}
+// import { useState } from "react";
+
+// function GetAllUsers(){
+//     const[users, setUsers] = useState([]);
+// }
+
+// function getUsers(){
+
+//     axios
+//       .get("http://localhost:8080/users")
+//       .then(function (response) {
+//         setUsers(response.data);
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//         setUsers([]);
+//       });
+// }
+
+
+export default function GetAllUsers() {}
--- src/pages/ReportView.js
+++ src/pages/ReportView.js
@@ -1,4 +1,32 @@
+import React from "react";
+
 export default function ReportView(){
     
-    console.log("Report Done");
+    //console.log("Report Done");
+
+    const handleUsersReport = () => {
+        console.log("Users Report");
+
+        //logic
+    }
+
+    const handleProductsReport = () => {
+        console.log("Products Report");
+
+        //logic
+    }
+
+    const buttonStyle = {
+        margin: '10px',
+        padding: '10px 20px',
+        fontSize: '16px',
+    };
+
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Report View</h1>
+            <button style={buttonStyle} onClick={handleUsersReport}>Users Report</button>
+            <button style={buttonStyle} onClick={handleProductsReport}>Products Report</button>
+        </div>
+    );
 }
